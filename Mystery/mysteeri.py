936696cd97694b57beb40fcd23adb0cd23a8bd46
Hogwarter <37118117+Hogwarter@users.noreply.github.com>
2022-11-24 17:08:45 +0200

    Add files via upload

---
+++ Mystery/mysteeri.py
@@ -0,0 +1,27 @@
+ur_name = input("What shall you be referd to as? (ex. Mrs. Fellow)")
+
+print("The murderd victim: Lord Robert Gallowgate")
+
+while True:
+    print("The suspects are:")
+    print("1. The wife, Lady Anne Gallowgate")
+    print("2. The cook, Marie Fraser")
+    print("3. The Butler, Charles Fraser")
+    talk_to = input(f"Who do you wish to converse with, {ur_name}?")
+    if talk_to == "1":
+        print("111")
+    if talk_to == "2":
+        print("111")
+    if talk_to == "3":
+        print("111")
+
+    break
+
+
+
+
+
+
+
+
+
