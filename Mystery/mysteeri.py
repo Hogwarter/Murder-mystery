3050ea77f3a88bb2dcceaf0ecff8ca31f63cd46b
Hogwarter <37118117+Hogwarter@users.noreply.github.com>
2022-11-24 17:15:48 +0200

    Update mysteeri.py

--- Mystery/mysteeri.py
+++ Mystery/mysteeri.py
@@ -1,5 +1,5 @@
 ur_name = input("What shall you be referd to as? (ex. Mrs. Fellow)")
-
+title = "detective"
 print("The murderd victim: Lord Robert Gallowgate")
 
 while True:
@@ -7,13 +7,14 @@ while True:
     print("1. The wife, Lady Anne Gallowgate")
     print("2. The cook, Marie Fraser")
     print("3. The Butler, Charles Fraser")
+    print("")
     talk_to = input(f"Who do you wish to converse with, {ur_name}?")
     if talk_to == "1":
-        print("111")
+        print(f"The wife: Yes? What did you need {ur_name}?")
     if talk_to == "2":
-        print("111")
+        print(f"The cook: Yes? {title}?")
     if talk_to == "3":
-        print("111")
+        print(f"The butler: How can I be of service {title}?")
 
     break
 
